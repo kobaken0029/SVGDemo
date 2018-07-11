1ee389359bfda21818fe3b8b13ddd773af2258f7
小林健人 <kobayashitaketo@kobayashitaketonoiMac-puro.local>
2018-07-11 18:08:00 +0900

    えいってやってばーん

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SVGDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SVGDemo
+  pod 'SVGKit', :git => 'https://github.com/SVGKit/SVGKit.git', :branch => '2.x'
+  pod 'PocketSVG', '~> 2.0'
+  pod "Macaw", "0.9.1"
+
+  target 'SVGDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SVGDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- SVGDemo/ViewController.swift
+++ SVGDemo/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import SVGKit
+import SwiftSVG
+import PocketSVG
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageContainer: UIView!
+    @IBOutlet weak var imageContainerPocket: UIView!
+    @IBOutlet weak var imageContainerSnowflake: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let _ = [imageView, imageContainer, imageContainerPocket, imageContainerSnowflake].map { setup($0) }
+        applySVGKit(to: imageView)
+        applySwiftSVG(to: imageContainer)
+        applyPocketSVG(to: imageContainerPocket)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setup(_ view: UIView) {
+        view.isHidden = false
+        view.backgroundColor = .clear
+        view.layer.borderWidth = 4
+        view.contentMode = .scaleAspectFit
     }
 
+    private func applySVGKit(to view: UIImageView) {
+        let pc = SVGKImage(named: "pc")
+        pc?.size = view.bounds.size
+        view.image = pc?.uiImage
+    }
 
+    private func applySwiftSVG(to view: UIView) {
+        let pc = UIView(SVGNamed: "test")
+        pc.backgroundColor = .clear
+        pc.frame = view.bounds
+        view.addSubview(pc)
+    }
+
+    private func applyPocketSVG(to view: UIView) {
+        let url = Bundle.main.url(forResource: "pc", withExtension: "svg")!
+        let svgImageView = SVGImageView(contentsOf: url)
+        svgImageView.backgroundColor = .clear
+        svgImageView.frame = view.bounds
+        svgImageView.contentMode = .scaleAspectFit
+        view.addSubview(svgImageView)
+    }
 }
-
